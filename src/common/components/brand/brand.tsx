227fdfcfe102648160122577954c9ea5b2d0f7c2
Marcus van Winden <marcus.van.winden@icloud.com>
2022-07-10 21:42:43 +0200

    feat: update brand link

--- src/common/components/brand/brand.tsx
+++ src/common/components/brand/brand.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import css from './brand.module.css';
 
 export const Brand: React.FC = () => (
   <h1 className={css.root}>
-    <a href="/">Foodly</a>
+    <Link to="/">Foodly</Link>
   </h1>
 );
